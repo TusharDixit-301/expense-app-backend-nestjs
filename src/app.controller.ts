1e28826f2250fc923b95e0999f39e2008ca1cda9
TusharDixit <tushar.dixit@wowlabz.com>
2023-08-26 04:14:42 +0530

    getReportById endpoint created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,16 +2,30 @@ import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ReportType, data } from './data';
 @Controller('/report/:type')
 export class AppController {
+  /**
+   * The `@Get()` decorator is used to define a GET endpoint in the controller
+   * In this case, it is defining a GET endpoint for retrieving all reports.
+   */
   @Get()
   getAllReports(@Param('type') type: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
     return data.report.filter((item) => item.type === reportType);
   }
+
+  /** 
+    * The `@Get(':id')` decorator is used to define a GET endpoint for retrieving a specific report by its ID. 
+    * The `@Param('id')` decorator is used to define a parameter in the endpoint URL.
+    * In this case, it is defining a parameter called `id` in the endpoint URL.
+  */
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(@Param('type') type: string, @Param('id') id: number) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const dataByType = data.report.filter((item) => item.type === reportType);
+    return dataByType.find((report) => report.id === Number(id));
   }
+  
   @Post()
   createReport() {
     return 'Created';
